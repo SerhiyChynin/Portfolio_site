9f9aac7a631cd899369746462de7c392501ef26f
SerhiiChynin <sergey.chinin@gmail.com>
2023-07-07 14:38:18 +0300

    services map

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -11,6 +11,27 @@ export const Services = () => {
                       <h3 className="text-brown">my <span className='text-dark'>services</span></h3>
                       <p className="text">I offer the right solutions foy your digital business.</p>
                   </div>
+                  <div className="services-list grid">
+                      {
+                          services.map((item, index) => {
+                              return (
+                                  <div className="services-item text-center" key={index}>
+                                      <div className="services-item-img">
+                                          <img src={item.image} className='mx-auto' />
+                                      </div>
+                                      <div className="services-item-text">
+                                          <h4 className='fs-22 fw-5 op-08'>
+                                              {item.title}
+                                          </h4>
+                                          <p className='text mx-auto'>
+                                              {item.paragraph}
+                                          </p>
+                                      </div>
+                                  </div>
+                              )
+                          })
+                      }
+                  </div>
               </div>
           </div>
     </div>
