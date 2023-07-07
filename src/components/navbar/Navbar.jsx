baac497b616ce79d90cbe4b14914d1d8743b6504
SerhiiChynin <sergey.chinin@gmail.com>
2023-07-07 14:09:08 +0300

    burger

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 export const Navbar = () => {
+    const [navToggler, setNavToggler] = useState(false);
+    const closeNavbar = () => setNavToggler(false);
   return (
       <nav className='navbar'>
           <div className='container'>
               <div className="navbar-content">
                   <div className='brand-and-toggler flex flex-sb'>
                       <Link to='/' className='navbar-brand text-uppercase fw-7 text-white ls-2 fs-22'>Serhii</Link>
-                      <button type='button' className='navbar-open-btn text-white'>
+                      <button type='button' className='navbar-open-btn text-white'
+                      onClick={()=>setNavToggler(!navToggler)}
+                      >
                           <FaBars size={30}/>
                       </button>
-                      <div className='navbar-collapse'>
-                          <button type='button' className='navbar-close-btn text-white'>
+                      <div className={navToggler ? 'navbar-collapse show-navbar-collapse' : 'navbar-collapse'}>
+                          <button type='button' className='navbar-close-btn text-white'
+                          onClick={closeNavbar}
+                          >
                               <FaTimes size={30}/>
                           </button>
                           <ul className='navbar-nav'>
