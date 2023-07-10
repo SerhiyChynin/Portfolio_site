4587c615de6bbb527a5cbdea031b2cce6eaac926
SerhiiChynin <sergey.chinin@gmail.com>
2023-07-10 18:45:44 +0300

    workProcess

---
+++ src/components/WorkProcess/WorkProcess.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { work_process } from '../../constants/data';
+import './WorkProcess.css';
+
+function WorkProcess() {
+  return (
+      <div className='workprocess section-p bg-white' id='workprocess'>
+          
+    </div>
+  )
+}
+
+export default WorkProcess
