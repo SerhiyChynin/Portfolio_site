ab28e806dcde4ae1c1b64e5876a6d913f03d9a2d
SerhiiChynin <sergey.chinin@gmail.com>
2023-07-11 11:57:01 +0300

    workprocess map

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './assets/js/script';
 import  Header  from './components/Header/Header';
 import { Services } from './components/Services/Services';
 import Work from './components/Work/Work';
+import WorkProcess from './components/WorkProcess/WorkProcess';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Services/>
-      <Work/>
+      <Work />
+      <WorkProcess/>
     </div>
   );
 }
--- src/components/WorkProcess/WorkProcess.jsx
+++ src/components/WorkProcess/WorkProcess.jsx
@@ -5,6 +5,30 @@ import './WorkProcess.css';
 function WorkProcess() {
   return (
       <div className='workprocess section-p bg-white' id='workprocess'>
+      <div className="container">
+        <div className="workprocess-content">
+          <div className="section-title">
+            <h3 className="text-brown">working <span className='text-dark'>process</span></h3>
+            <p className="text">I order the right solutions to your digital business.</p>
+          </div>
+
+          <div className="work-process-list">
+            {
+              work_process.map((item, index) => {
+                return (
+                  <div className="workprocess-item text-center" key={index}>
+                    <div className="workprocess-item-title flex">
+                      <span className="text-brown fw-7">0{index + 1} </span>
+                      <h3 className="text-dark fw-5">{item.title}</h3>
+                    </div>
+                    <p className="text">{ item.paragraph}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
           
     </div>
   )
